b6f8bf75d880edcdd7a1ccd36e5d2e4646bb29f8
Symbol Wu <bluessoul@sina.com>
2020-11-30 17:15:28 +0800

    初始化构建代码

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 const express = require("express");
-
 // Set up the express app
 const app = express();
+const { execFile } = require("child_process");
+const path = require("path");
+// const pack = require("./build");
+// let in_process = false;
 
-let in_process = false;
-
-// get all todos
-app.get("/api/v1/todos", (req, res) => {
-  if (in_process) {
-    res.status(500).send({
-      success: "false",
-      message: "todos retrieved successfully",
-    });
-  }
-  res.status(200).send({
-    success: "true",
-    message: "todos retrieved successfully",
+app.post("/api/gatsbyBuild", async (req, res) => {
+  // const result = await pack();
+  // console.log(result);
+  // if (!result.success) {
+  //   res.status(500).send(result);
+  //   return;
+  // }
+  // res.status(200).send(result);
+  const sh = execFile(
+    "gatsby",
+    ["build"],
+    {
+      cwd: path.resolve(__dirname, "../cflp-spike-client"),
+    },
+    (error, stdout, stderr) => {
+      if (error) {
+        res.status(500).send({ message: "Build fail", success: false });
+        console.log("build error:", error);
+        return;
+      }
+      res.status(200).send({ message: "Build success", success: true });
+      console.log("build success:", stdout);
+    }
+  );
+  sh.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
   });
 });
 
-const PORT = 5000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
